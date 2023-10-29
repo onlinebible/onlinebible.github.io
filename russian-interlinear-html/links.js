fab7d5cd49d96e1798a002ab9737c5ad0ae75016
Michael Vlasenko <Michael1972@mail.ru>
2023-10-29 10:34:41 +0600

    Changes for russian-interlinear-html

--- russian-interlinear-html/links.js
+++ russian-interlinear-html/links.js
@@ -1,6 +1,7 @@
 ﻿var BooksRus = 'Быт Исх Лев Чис Втор Нав Суд Руф 1Цар 2Цар 3Цар 4Цар 1Пар 2Пар Езд Нее Есф Иов Пс Прит Екк Песн Ис Иер Плач Иез Дан Ос Иоил Ам Авд Иона Мих Наум Авв Соф Агг Зах Мал';
 var BooksEng = 'Ge Ex Le Nu De Jos Jud Ru 1Sa 2Sa 1Ki 2Ki 1Ch 2Ch Ezr Ne Es Job Ps Pr Ec So Isa Jer La Eze Da Ho Joe Am Ob Jon Mic Na Hab Zep Hag Zec Mal';
-arrRus = BooksRus.split(' '); arrEng = BooksEng.split(' ');
+arrRus = BooksRus.split(' '); 
+arrEng = BooksEng.split(' ');
 arrRusEng = new Array();
 for (i=0;i<arrRus.length;i++) { arrRusEng[arrRus[i]] = arrEng[i]; }
 
@@ -15,6 +16,7 @@ function process_click(e) {
 	var URL = location.pathname;
 	arr = URL.split('/'); 
 	DirName = arr[arr.length-2];
+	book = 0;
 	for (i=0;i<arrEng.length;i++) { if (arrEng[i] == DirName) { book = i + 1 } };
 	if (book < 40) { letter = 'h' } else  { letter = 'g' };
 	if (ref < 1000) { letter = letter + '0' };
