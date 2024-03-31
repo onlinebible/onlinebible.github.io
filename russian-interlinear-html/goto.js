063b62ad93850b374715d85d7ac8e241bcb0c95b
Michael Vlasenko <Michael1972@mail.ru>
2024-03-31 13:06:58 +0600

    Changes for russian-interlinear-html

--- russian-interlinear-html/goto.js
+++ russian-interlinear-html/goto.js
@@ -10,7 +10,6 @@
 function ShowLocator() {
 if (BookShortName) {
 	var SectionsCount = 0;
-	document.write('<BUTTON onclick="location=document.getElementById(\'LocatorSections\').value;">Перейти</BUTTON>\n');
 	document.write('<SELECT id="LocatorWorks" size="1" onChange="LocatorChange();" name="LocatorWorks">\n');
 	for (i=0;i<arrWorksDisplay.length;i++) {
 		document.write("<OPTION value='"+arrWorksLink[i]+" "+arrWorksSections[i]+"'");
@@ -22,6 +21,7 @@ if (BookShortName) {
 	} // end for
 	document.write("</SELECT>\n");
 	document.write('<SELECT id="LocatorSections" size="1" name="LocatorSections"></SELECT>\n');
+	document.write('<BUTTON onclick="location=document.getElementById(\'LocatorSections\').value;">Перейти</BUTTON>\n');
 	LocatorChange();
 } // end if
 } // end function
--- russian-interlinear-html/header.js
+++ russian-interlinear-html/header.js
@@ -1,11 +1,11 @@
 ﻿function Header () {
   document.write("<TABLE cellspacing='0' bgcolor='#66CCFF' width='100%'><TR>");
   document.write("<TD width='1' title='Предыдущая глава'>");
-	if (Back == '') { document.write("<BUTTON disabled>Назад</BUTTON>"); }
-	else { document.write("<BUTTON onClick=\"window.location='"+Back+"'\">Назад</BUTTON>"); }
+	if (Back == '') { document.write("<BUTTON disabled>&laquo;</BUTTON>"); }
+	else { document.write("<BUTTON onClick=\"window.location='"+Back+"'\">&laquo;</BUTTON>"); }
   document.write("</TD><TD><DIV id='header'><A href='"+Up+"' title='Оглавление'>"+Name+"</A></DIV></TD>");
   document.write("<TD width='1' title='Следующая глава'>");
-	if (Next == '') { document.write("<BUTTON disabled>Вперед</BUTTON>"); }
-	else { document.write("<BUTTON onClick=\"window.location='"+Next+"'\">Вперед</BUTTON>"); }
+	if (Next == '') { document.write("<BUTTON disabled>&raquo;</BUTTON>"); }
+	else { document.write("<BUTTON onClick=\"window.location='"+Next+"'\">&raquo;</BUTTON>"); }
   document.write("</TD></TR></TABLE>");
 }
