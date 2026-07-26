a4b1e5b9008e941fd02029373cd01b4b808ff332
Michael Vlasenko <Michael1972@mail.ru>
2026-07-26 23:17:30 +0500

    Веб-страница обновлена: тест с katalog.html - поиск источника неполадок TEST 1

--- js/imageviewer.js
+++ js/imageviewer.js
@@ -8,6 +8,15 @@ $(function () {
 
 $(".wp-colorbox-iframe").colorbox({
     iframe: true,
-    width: "90%",
-    height: "90%"
+
+    width: "95%",
+    height: "95%",
+
+    maxWidth: "95%",
+    maxHeight: "95%",
+
+    reposition: true,
+    fixed: true,
+
+    scrolling: true
 });
